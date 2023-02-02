691ed8cc5999bb8994626e8399ea6440a9bfc18b
Akshay-Durgade <113453627+Akshay-Durgade@users.noreply.github.com>
2023-02-02 14:51:40 +0530

    second commit

--- Git.java
+++ Git.java
@@ -21,6 +21,26 @@ class Git
 					second=third;
 				}
 			break;
+            case 2:
+				int m1=20;
+				boolean flag=true;
+				for(int i=2;i<=m1;i++)
+				{
+					flag=false;
+					if(i==2)
+						System.out.print(i+",");
+					else
+					{
+						for(int j=2;j<i;j++)
+						{
+							if(i%j==0)
+								flag=true;
+						}
+						if(!flag)
+							System.out.print(i+",");
+					}
+				}
+			break;
 		}
     }
 }
